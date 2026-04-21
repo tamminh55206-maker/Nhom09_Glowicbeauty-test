dba568b5e0255e4bd4e21f5e0a770320bc59af17
MinhTam <tammminh55206@gmail.com>
2026-04-21 22:42:57 +0700

    update fullstack

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-    ],
+    unoptimized: true, // ← fix hết tất cả lỗi domain 1 lần
   },
 };
 
